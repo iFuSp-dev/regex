03c280bd9e65fcdfeacd4c9a37f30b72e91b6467
Ismael Garcia <ismaelgarciap@outlook.com>
2024-07-16 13:48:54 +0200

    wildcharacters en match. varios posibles caracteres definidos en una posicion del match

--- regex_1.js
+++ regex_1.js
@@ -2,4 +2,4 @@ let prueba = "hola Hola hielo hule hilo halo helio ola HoLa"; //Creo un String
 
 
 
-console.log(prueba.match(/hola/gi))
+console.log(prueba.match(/h[aeiou]l[aeiou]/gi))  //Los corchetes indican las posibles wildcards en esa posicion
